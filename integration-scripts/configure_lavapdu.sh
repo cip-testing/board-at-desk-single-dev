af98fc19b7718d9ed7e0c58c301db4820eced29b
Don Brown <don.f.brown@comcast.net>
2017-03-29 22:14:19 +0000

    Initial commit to copy lavapdu drivers & commands into their proper location.

---
+++ integration-scripts/configure_lavapdu.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+# Copyright (C) 2017, Codethink, Ltd., Don Brown
+# <don.brown@codethink.co.uk>
+# Configure KernelCI for 'Single Developer at Desk with Board' Use Case
+
+# Copy drivers to /usr/lib/python2.7/dist-packages/lavapdu/drivers
+sudo DEBIAN_FRONTEND=noninteractive cp /vagrant/scripts/PDU/*.py /usr/lib/python2.7/dist-packages/lavapdu/drivers
+
+# Move relay-ctrl.py to /usr/bin
+sudo DEBIAN_FRONTEND=noninteractive mv /usr/lib/python2.7/dist-packages/lavapdu/drivers/relay-ctrl.py /usr/bin
