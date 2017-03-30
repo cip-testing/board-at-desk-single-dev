15689c1163629c5953a347b807dccd94c271953b
Robert Marshall <robert.marshall@codethink.co.uk>
2017-03-30 13:16:41 +0100

    cleanup the import script

--- importbox.sh
+++ importbox.sh
@@ -9,14 +9,19 @@ box=$1
 #dir=importBox
 #mkdir $dir
 #cd $dir
-# importedBox is another parameter
+# importedBox is another parameter?
 vagrant box add importedBox $1
 if [ ! -f Vagrantfile ] ; then
     vagrant init importedBox
     git clone https://gitlab.com/cip-project/board-at-desk-single-dev.git
     rm Vagrantfile
     cp board-at-desk-single-dev/Vagrantfile .
+    mv board-at-desk-single-dev/integration-scripts .
+    # integration-scripts only needed because we're using the Vagrantfile as it stands
+    # and provisioning will then spot that this is an already provisioned box
+    rm -rf board-at-desk-single-dev
 else
+    # it is probably there because we're running this script also from git
     mv -f Vagrantfile Vagrantfile-tmp
     vagrant init importedBox
     mv Vagrantfile-tmp Vagrantfile
@@ -24,4 +29,3 @@ fi
 # are any mods necessary?
 vagrant up
 echo new KernelCI board at desk ready for use
-# in $dir
