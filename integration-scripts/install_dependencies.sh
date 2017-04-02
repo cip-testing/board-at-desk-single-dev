2662454899796a3a1867bafbc594a2f6a7d6cb20
Don Brown <don.f.brown@comcast.net>
2017-04-02 20:12:47 +0000

    Update install_dependencies.sh - Add AGPL-3.0 License

--- integration-scripts/install_dependencies.sh
+++ integration-scripts/install_dependencies.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 # Copyright (C) 2016, Siemens AG, Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
-# SPDX-License-Identifier:	Apache-2.0
+# SPDX-License-Identifier:	AGPL-3.0
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 echo "START: install_dependencies.sh"
 
 set -e
